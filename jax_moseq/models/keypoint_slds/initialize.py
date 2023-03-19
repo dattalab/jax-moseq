now using float32 by default, checks args for correct precision

jax_moseq/models/keypoint_slds/initialize.py
@@ -3,7 +3,7 @@ import jax.numpy as jnp
 import jax.random as jr
 
 from jax_moseq import utils
-from jax_moseq.utils import jax_io, device_put_as_scalar
+from jax_moseq.utils import jax_io, device_put_as_scalar, check_precision
 
 from jax_moseq.models import arhmm, slds
 from jax_moseq.models.keypoint_slds.gibbs import resample_scales
@@ -315,14 +315,16 @@ def estimate_error(conf, slope, intercept):
     return 10 ** (2 * (jnp.log10(conf + 1e-6) * slope + intercept))
 
 
+@check_precision
 def _check_init_args(data, states, params, hypparams,
                      trans_hypparams, ar_hypparams,
                      obs_hypparams, cen_hypparams,
                      has_conf, noise_prior, error_estimator,
                      anterior_idxs, posterior_idxs):
     """
-    Helper method for :py:func:`jax_moseq.models.initialize.init_model` that ensures a sufficient subset
-    of the initialization arguments have been provided by the caller.
+    Helper method for :py:func:`jax_moseq.models.initialize.init_model` 
+    that ensures a sufficient subset of the initialization arguments have 
+    been provided by the caller.
     
     Parameters
     ----------

jax_moseq/utils/utils.py
@@ -4,7 +4,9 @@ import jax.numpy as jnp
 from jax.tree_util import tree_map
 from sklearn.decomposition import PCA
 from jax.scipy.linalg import cho_factor, cho_solve
-
+import inspect
+import functools
+from textwrap import fill
 
 def psd_solve(A, B, diagonal_boost=1e-6):
     """
@@ -185,6 +187,46 @@ def batch(data_dict, keys=None, seg_length=None, seg_overlap=30):
     stack = np.stack(stack)
     mask = np.stack(mask)
     return stack,mask,labels
+
+
+def _check_array_precision(arg, x64):
+    """Checks if precision of `arg` matches `x64`"""
+    permitted_dtypes = [
+        (np.int64 if x64 else np.int32),
+        (jnp.int64 if x64 else jnp.int32),
+        (np.float64 if x64 else np.float32),
+        (jnp.float64 if x64 else jnp.float32)]
+    if isinstance(arg, jnp.ndarray) or isinstance(arg, np.ndarray):
+        if not arg.dtype in permitted_dtypes:  return False   
+    return True
+
+def check_precision(fn):
+    """
+    Decorator to check that the precision of the arguments matches the
+    precision of the jax configuration.
+    """
+    arg_names = inspect.getfullargspec(fn).args
+    x64 = jax.config.x64_enabled
+    
+    @functools.wraps(fn)
+    def wrapper(*args, **kwargs):
+        
+        args_with_wrong_precision = []
+        for name,arg in list(zip(arg_names,args)) + list(kwargs.items()):
+            check_fn = functools.partial(_check_array_precision, x64=x64)
+            if not jax.tree_util.tree_all(jax.tree_map(check_fn, arg)):
+                args_with_wrong_precision.append(name)
+                
+        if len(args_with_wrong_precision) > 0:
+            msg = f'JAX is configured to use {"64" if x64 else "32"}-bit precision, '
+            msg += f'but following arguments contain {"32" if x64 else "64"}-bit arrays: '
+            msg += ', '.join([f'"{name}"' for name in args_with_wrong_precision])
+            msg += '. Either change the JAX config using `jax.config.update("jax_enable_x64", True/False)` '
+            msg += 'or convert the arguments to the correct precision using `jax_moseq.utils.utils.convert_data_precision`.'
+            raise ValueError(msg)
+
+        return fn(*args, **kwargs)
+    return wrapper
 
 def convert_data_precision(data, x64=None):
     """