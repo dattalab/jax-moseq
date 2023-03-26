6e3837b77d127270a50c94df336e0675f4b0c0c1
Caleb Weinreb <calebsw@gmail.com>
2023-03-26 01:27:50 -0400

    require python 3.8

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3',
         'Operating System :: OS Independent'
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     install_requires=[
         'numba',
         'dynamax',
