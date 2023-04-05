1f321c418caa66646c750ddb8e460c4c2f019928
Caleb Weinreb <calebsw@gmail.com>
2023-04-05 09:19:10 -0400

    first glm_hmm commit

--- jax_moseq/utils/utils.py
+++ jax_moseq/utils/utils.py
@@ -117,6 +117,28 @@ def fit_pca(Y, mask, PCA_fitting_num_frames=1000000,
     return pca
 
 
+def pad_along_axis(arr, pad_widths, axis=0, value=0):
+    """
+    Pad an array along a single axis
+
+    Parameters
+    -------
+    arr: ndarray, Array to be padded
+    pad_widths: tuple (int,int), Amount of padding on either end
+    axis: int, Axis along which to add padding
+    value: float, Value of padded array elements
+
+    Returns
+    _______
+    padded_arr: ndarray
+
+    """
+    pad_widths_full = [(0,0)]*len(arr.shape)
+    pad_widths_full[axis] = pad_widths
+    padded_arr = np.pad(arr, pad_widths_full, constant_values=value)
+    return padded_arr
+
+
 
 def unbatch(data, labels): 
     """
