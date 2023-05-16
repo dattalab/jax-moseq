6576a3d4520b52dd395ddb6d0d483421bfa837f5
Caleb Weinreb <calebsw@gmail.com>
2023-05-16 11:30:03 -0400

    tiny code simplification

--- jax_moseq/models/arhmm/gibbs.py
+++ jax_moseq/models/arhmm/gibbs.py
@@ -53,11 +53,9 @@ def resample_discrete_stateseqs(seed, x, mask, Ab, Q, pi, **kwargs):
         Discrete state sequences.
     """
     nlags = get_nlags(Ab)
-    num_samples = mask.shape[0]
-
     log_likelihoods = jax.lax.map(partial(ar_log_likelihood, x), (Ab, Q))
     _, z = jax.vmap(sample_hmm_stateseq, in_axes=(0,na,0,0))(
-        jr.split(seed, num_samples),
+        jr.split(seed, mask.shape[0]),
         pi,
         jnp.moveaxis(log_likelihoods,0,-1),
         mask.astype(float)[:,nlags:])
